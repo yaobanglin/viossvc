f9e8a1581a0deaca2da93777560ee4af5944c167
xiaomudegithub <xmdltyx@163.com>
2016-11-25 10:44:47 +0800

    加上重置成功提示

--- viossvc/Scenes/User/ResetPasswordViewController.swift
+++ viossvc/Scenes/User/ResetPasswordViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SVProgressHUD
 class ResetPasswordViewController: BaseTableViewController {
     
     @IBOutlet weak var oldPasswordText: UITextField!
@@ -18,11 +18,16 @@ class ResetPasswordViewController: BaseTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        SVProgressHUD.dismiss()
+    }
     
     @IBAction func resetPasswordBtnTapped(sender: UIButton) {
         if checkTextFieldEmpty([oldPasswordText,newPasswordText]) {
             if newPasswordText.text == confirmPasswordText.text  {
                 hideKeyboard()
+                SVProgressHUD.showWithStatus("密码重置中...")
                 AppAPIHelper.userAPI().nodifyPasswrod(CurrentUserHelper.shared.userInfo.uid, oldPassword: oldPasswordText.text!, newPasword: newPasswordText.text!, complete: { [weak self](model) in
                     if let strongSelf = self {
                         let modelDic = model as? Dictionary<String, AnyObject>
@@ -38,7 +43,7 @@ class ResetPasswordViewController: BaseTableViewController {
     
     
     func didResetPasswordComplete(dict:Dictionary<String,AnyObject>!) {
-        showWithStatus("设置成功")
-       navigationController?.popViewControllerAnimated(true)
+        SVProgressHUD.showSuccessWithStatus("重置成功")
+        navigationController?.popViewControllerAnimated(true)
     }
 }
