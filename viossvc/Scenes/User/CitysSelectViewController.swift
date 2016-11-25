24b596ddbc5dd0263c81277bcb629ca5118b3310
xiaomudegithub <xmdltyx@163.com>
2016-11-25 13:58:55 +0800

    加入常住地址选择

---
+++ viossvc/Scenes/User/CitysSelectViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CitysSelectViewController.swift
+//  viossvc
+//
+//  Created by 木柳 on 2016/11/25.
+//  Copyright © 2016年 com.yundian. All rights reserved.
+//
+
+import UIKit
+
+class CitysSelectViewController: BaseTableViewController {
+
+}
