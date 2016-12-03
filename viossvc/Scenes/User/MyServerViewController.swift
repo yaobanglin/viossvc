ab3b3ae8e97257672af43c8df5f86dd3aca90296
xiaomudegithub <xmdltyx@163.com>
2016-12-03 16:08:58 +0800

    标签管理接入

--- viossvc/Scenes/User/MyServerViewController.swift
+++ viossvc/Scenes/User/MyServerViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class MyServerViewController: BaseTableViewController {
+class MyServerViewController: BaseTableViewController, LayoutStopDelegate {
     @IBOutlet weak var zhimaAnthIcon: UIImageView!
     @IBOutlet weak var idAuthIcon: UIImageView!
     @IBOutlet weak var headerImage: UIImageView!
     @IBOutlet weak var headerContent: UIView!
     @IBOutlet weak var picAuthLabel: UILabel!
     @IBOutlet weak var bgImage: UIImageView!
+    @IBOutlet weak var skillView: SkillLayoutView!
     @IBOutlet weak var serverTabel: ServerTableView!
     @IBOutlet weak var serverTabelCell: UITableViewCell!
     @IBOutlet weak var pictureCollection: UserPictureCollectionView!
@@ -35,6 +36,37 @@ class MyServerViewController: BaseTableViewController {
     }
     //MARK: --DATA
     func initData() {
+        //我的标签
+        AppAPIHelper.orderAPI().getSkills({[weak self] (response) in
+            let array = response as? Array<SkillsModel>
+            var skillDict: Dictionary<Int, AnyObject> = [:]
+            for skill in array! {
+                let size = skill.skill_name!.boundingRectWithSize(CGSizeMake(0, 21), font: UIFont.systemFontOfSize(15), lineSpacing: 0)
+                skill.labelWidth = size.width + 30
+                skillDict[skill.skill_id] = skill
+            }
+            
+            AppAPIHelper.userAPI().getOrModfyUserSkills(0, skills: "", complete: { (response) in
+                
+                if response != nil {
+                    let dict = response as! Dictionary<String, AnyObject>
+                    CurrentUserHelper.shared.userInfo.skills = dict["skills_"] as? String
+                    let skillArray = CurrentUserHelper.shared.userInfo.skills?.componentsSeparatedByString(",")
+                    if  skillArray?.count == 0{
+                        return
+                    }
+                    var skillModelArray: [SkillsModel] = []
+                    for skillName in skillArray!{
+                        if Int(skillName) > 0 {
+                            let model = skillDict[Int(skillName)!] as! SkillsModel
+                            skillModelArray.append(model)
+                        }
+                    }
+                    self?.skillView.dataSouce = skillModelArray
+                }
+            }, error: (self?.errorBlockFunc())!)
+          
+        }, error: errorBlockFunc())
         //我的服务
         AppAPIHelper.userAPI().serviceList({ [weak self](result) in
             if result == nil {
@@ -54,7 +86,9 @@ class MyServerViewController: BaseTableViewController {
     }
     //MARK: --UI
     func initUI() {
-        
+        skillView.delegate = self
+        skillView.showDelete = false
+        skillView.collectionView?.backgroundColor = UIColor(RGBHex: 0xf2f2f2)
     }
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         if indexPath.section == 0 {
@@ -77,5 +111,8 @@ class MyServerViewController: BaseTableViewController {
             controller.serverData = serverData
         }
     }
-    
+    func layoutStopWithHeight(layoutView: SkillLayoutView, height: CGFloat) {
+        markHeight = height
+        tableView.reloadData()
+    }
 }
