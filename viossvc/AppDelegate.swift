37855a4547dbb6c186e75950f53fa4813ec4ab64
xiaomudegithub <xmdltyx@163.com>
2016-12-06 16:47:56 +0800

    继承Fabric

--- viossvc/AppDelegate.swift
+++ viossvc/AppDelegate.swift
@@ -9,6 +9,10 @@
 import UIKit
 import XCGLogger
 import SVProgressHUD
+import Fabric
+import Crashlytics
+
+ 
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         appearance()
         SocketRequestManage.shared.start()
         pushMessageRegister()
