bb2ae20aa9d4a00b3352e6bb5492c278ce627c7b
司留梦 <siliumeng@siliumengdeMacBook-Pro.local>
2016-12-30 13:27:59 +0800

    change show frame in storyboard

--- viossvc/Scenes/User/ServerTableView.swift
+++ viossvc/Scenes/User/ServerTableView.swift
@@ -13,73 +13,6 @@ class ServerCell: UITableViewCell {
     @IBOutlet weak var serverNameLabel: UILabel!
     @IBOutlet weak var serverTimeLabel: UILabel!
     @IBOutlet weak var serverPriceLabel: UILabel!
-//    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        var upview = contentView.viewWithTag(1000)
-//        if upview == nil {
-//            upview = UIView()
-//            upview?.tag = 1000
-//            upview?.backgroundColor = UIColor.grayColor()
-//            contentView.addSubview(upview!)
-//            upview?.snp_makeConstraints(closure: { (make) in
-//                make.left.equalTo(contentView).offset(10)
-//                make.right.equalTo(contentView).offset(-10)
-//                make.top.equalTo(contentView)
-//                make.height.equalTo(0.5)
-//            })
-//        }
-//        var timeLab = contentView.viewWithTag(1001) as? UILabel
-//        if timeLab == nil {
-//            timeLab = UILabel()
-//            timeLab?.tag = 1001
-//            timeLab?.font = UIFont.systemFontOfSize(13.0)
-//            contentView.addSubview(timeLab!)
-//            timeLab?.snp_makeConstraints(closure: { (make) in
-//                make.top.equalTo(contentView).offset(5)
-//                make.bottom.equalTo(contentView).offset(-5)
-//                make.centerX.equalTo(contentView)
-//                make.width.equalTo(80)
-//            })
-//        }
-//        
-//        var nameLab = contentView.viewWithTag(1002) as? UILabel
-//        if nameLab == nil {
-//            nameLab = UILabel()
-//            nameLab?.tag = 1002
-//            //            nameLab?.numberOfLines = 0
-//            //            nameLab?.lineBreakMode = .ByWordWrapping
-//            nameLab?.font = UIFont.systemFontOfSize(13.0)
-//            contentView.addSubview(nameLab!)
-//            nameLab?.snp_makeConstraints(closure: { (make) in
-//                make.left.equalTo(contentView).offset(10)
-//                make.top.equalTo(contentView).offset(5)
-//                make.bottom.equalTo(contentView).offset(-5)
-//                make.right.equalTo(timeLab!.snp_left).offset(-10)
-//            })
-//        }
-//        
-//        var priceLab = contentView.viewWithTag(1003) as? UILabel
-//        if priceLab == nil {
-//            priceLab = UILabel()
-//            priceLab?.tag = 1003
-//            priceLab?.textAlignment = .Right
-//            priceLab?.font = UIFont.systemFontOfSize(13.0)
-//            contentView.addSubview(priceLab!)
-//            priceLab?.snp_makeConstraints(closure: { (make) in
-//                make.left.equalTo(timeLab!.snp_right).offset(10)
-//                make.top.equalTo(contentView).offset(5)
-//                make.bottom.equalTo(contentView).offset(-5)
-//                make.right.equalTo(contentView).offset(-10)
-//            })
-//            
-//        }
-//    
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
 }
 
 class ServerTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
@@ -87,10 +20,6 @@ class ServerTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        // Nib 注册
-//        self.registerNib(UINib(nibName: "serviceCell", bundle: nil), forCellReuseIdentifier: "ServerCell")
-        // Class 注册
-//        self.registerClass(ServerCell.self, forCellReuseIdentifier: ServerCell.className())
         delegate = self
         dataSource = self
         scrollEnabled = false
@@ -98,22 +27,14 @@ class ServerTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
     
     //MARK: --delegate and datasource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return serverData.count
+        return 21//serverData.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: ServerCell = tableView.dequeueReusableCellWithIdentifier(ServerCell.className()) as! ServerCell
-        let model = serverData[indexPath.row]
-//        let serverNameLabel = cell.contentView.viewWithTag(1002) as? UILabel
-//        serverNameLabel?.text = model.service_name
-//        let serverPriceLabel = cell.contentView.viewWithTag(1003) as? UILabel
-//        serverPriceLabel?.text = "￥\(Double(model.service_price)/100)"
-//        let serverTimeLabel = cell.contentView.viewWithTag(1001) as? UILabel
-//        serverTimeLabel?.text = "\(time(model.service_start))--\(time(model.service_end))"
-//        let upLine = cell.contentView.viewWithTag(1000)
-//        upLine?.hidden = indexPath.row == 0
-        cell.serverNameLabel.text = model.service_name
-        cell.serverPriceLabel.text = "￥\(Double(model.service_price)/100)"
-        cell.serverTimeLabel.text = "\(time(model.service_start))--\(time(model.service_end))"
+//        let model = serverData[indexPath.row]
+        cell.serverNameLabel.text = indexPath.row/2==0 ? "asjdhagskdjs这是一个测试" : "这是一个测试"//model.service_name
+        cell.serverPriceLabel.text = "￥10000.00"//"￥\(Double(model.service_price)/100)"
+        cell.serverTimeLabel.text = "12:00-24:00"//"\(time(model.service_start))--\(time(model.service_end))"
         cell.upLine.hidden = indexPath.row == 0
         return cell
     }
